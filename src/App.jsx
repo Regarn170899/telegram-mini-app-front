7c139a0e5b0c0a9c47ae96aa7c9cfd909c8a16d8
@a.golovushkin <a.golovushkin@solber.ru>
2025-01-23 01:22:10 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
 import React from "react";
-import { HashRouter, Route, Routes} from "react-router-dom";
-import MainPage from "./components/mainPage.jsx";
 import Auth from "./components/Auth.jsx";
 
 function App() {
-    return(
-        <HashRouter >
-                <Routes>
-                    <Route path="/" element={<MainPage/>} />
-                    <Route path="/auth" element={<Auth />} />
-                </Routes>
-        </HashRouter >
+    return(<><Auth /></>
     )
 }
 
